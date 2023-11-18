acac7e0ea4e6bde2481af7f6d696ba9b1eab6360
James Kofi Myers <myers_james59@yahoo.com>
2023-11-18 20:05:04 +0000

    update dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
+import "./Dashboard.css";
 
 const Dashboard = () => {
   const { user } = useAuth();
